4f7688f6c8a8129be119fa4740fb104072706a4a
hicorelearningapp <hicorelearningapp@gmail.com>
2026-08-27 12:42:21 +0530

    Create customer.py

---
+++ Backend/backend_app/modules/ecommerce/schemas/customer.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import date
+
+class EcommerceCustomerBase(BaseModel):
+    PhoneNumber: str
+    ProfileName: Optional[str] = None
+    Email: Optional[EmailStr] = None
+    Gender: Optional[str] = None
+    DOB: Optional[date] = None
+    Address: Optional[str] = None
+    City: Optional[str] = None
+    State: Optional[str] = None
+    Pincode: Optional[str] = None
+
+class EcommerceCustomerCreate(EcommerceCustomerBase):
+    pass
+
+class EcommerceCustomerUpdate(BaseModel):
+    ProfileName: Optional[str] = None
+    Email: Optional[EmailStr] = None
+    Gender: Optional[str] = None
+    DOB: Optional[date] = None
+    Address: Optional[str] = None
+    City: Optional[str] = None
+    State: Optional[str] = None
+    Pincode: Optional[str] = None
+
+class EcommerceCustomerOut(EcommerceCustomerBase):
+    CustomerId: str
+    ProfileId: str
+
+    class Config:
+        from_attributes = True
