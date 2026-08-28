1909d0877e0ca5acd14aa4ae5779d7f2ad47e1b4
hicorelearningapp <hicorelearningapp@gmail.com>
2026-08-28 12:03:16 +0530

    Update database.py

--- Workflows/core/database.py
+++ Workflows/core/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 import os
 
 DB_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "workflows_session.db")
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
+SQLALCHEMY_DATABASE_URL = os.getenv("WORKFLOWS_DATABASE_URL", f"sqlite:///{DB_PATH}")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
