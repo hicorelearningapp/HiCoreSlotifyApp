f506a83fc7d705c2a7fcbbe481144901763ff9c7
hicorelearningapp <hicorelearningapp@gmail.com>
2026-09-12 20:27:15 +0530

    Update session_service.py

--- Workflows/core/services/session_service.py
+++ Workflows/core/services/session_service.py
@@ -63,7 +63,7 @@ class SessionService:
         session = self.get_session(phone_number, business_phone_number)
 
         if not session or not session.StateData:
-            
+            from core.SequenceFactory import SequenceFactory
             industry = SequenceFactory().getIndustry(business_phone_number)
 
             initial_state = {
