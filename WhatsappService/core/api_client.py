6552be28f6ad1d9206f0888a6b609bac505e1016
hicorelearningapp <hicorelearningapp@gmail.com>
2026-09-16 11:13:09 +0530

    workflow update -- Ajith

--- WhatsappService/core/api_client.py
+++ WhatsappService/core/api_client.py
@@ -36,10 +36,7 @@ class BackendAPIClient:
     def get_industry_config_by_phone(self, phone_number: str):
         return self._request("GET", f"/businesses/config/{phone_number}")
 
-    def get_product_config_by_phone(self, product_id: str):
-        return self._request("GET", f"/ecommerce/products/{product_id}/product-workflow")
-
-    def get_industry_config_by_product_id(self, product_id: str):
+    def get_product_config_by_id(self, product_id: str):
         return self._request("GET", f"/ecommerce/products/{product_id}/product-workflow")
 
     def get_industry_by_phone(self, phone_number: str):
